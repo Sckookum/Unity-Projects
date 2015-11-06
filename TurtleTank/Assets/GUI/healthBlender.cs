c3064587916e4104c60e72c9bcc9ce932df740d3
Jonathan Marshall <10588284@CS518-04.ad.uvu.edu>
2015-11-05 17:24:42 -0700

    Health bar implemented. Yay.

---
+++ TurtleTank/Assets/GUI/healthBlender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthBlender : MonoBehaviour {
+	Animator anim;
+	public static float health = 100f;
+	float maxHealth = 100f;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) 
+		{
+			health += 15;
+		}
+
+	if (Input.GetKeyDown (KeyCode.K)) 
+		{
+			health -= 15;
+		}
+
+		if (health >= maxHealth) 
+		{
+			health = maxHealth;
+		}
+		if (health < 0)
+		{
+			health = 0;
+		}
+		anim.SetFloat ("HealthBlend", health);
+	}
+	//use to call in other scripts.
+	/*if()
+	{
+		healthBlender.health -= 10;
+	}*/
+}
